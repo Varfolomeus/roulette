58f2d7dec00f9d75e47821db97b52ec58e81d7e1
Varfolomeus <gelohc@ukr.net>
2023-06-28 13:19:12 +0300

    fixed state issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./styles.css";
-import React from "react";
-import { makeAutoObservable } from "mobx";
-import { observer } from "mobx-react-lite";
+import './styles.css';
+import React from 'react';
+import { makeAutoObservable } from 'mobx';
+import { observer } from 'mobx-react-lite';
 
 class RouletteContext {
   rotateAngle = 0;
@@ -13,7 +13,6 @@ class RouletteContext {
   evenBid = 0;
   oddBid = 0;
   win = null;
-  wheelRef = null;
   gameOver = true;
   double = false;
 
@@ -39,16 +38,13 @@ class RouletteContext {
   setSpinStarted = (value) => {
     this.spinStarted = value;
   };
-  setwheelref = (value) => {
-    this.wheelRef = value;
-  };
   setAngleChanger = (value) => {
     this.angleChanger = value;
   };
   setBids = (bid, style) => {
-    if (style === "double") {
+    if (style === 'double') {
       this[bid] *= 2;
-    } else if (style === "zero") {
+    } else if (style === 'zero') {
       this[bid] = 0;
     } else {
       this[bid] += 1;
@@ -57,16 +53,13 @@ class RouletteContext {
   setGameOver = (value) => {
     this.gameOver = value;
   };
-  setRouletteStyle = (type, value) => {
-    this.wheelRef.style[type] = value;
-  };
   setDouble = () => {
     this.double = !this.double;
   };
 }
 const rouletteStorage = new RouletteContext();
 
-export const Tableau = observer(() => {
+export const Tableau = observer(({ wheelRef }) => {
   const {
     double,
     setDouble,
@@ -77,28 +70,69 @@ export const Tableau = observer(() => {
     setBids,
     spinStarted,
     setSpinStarted,
-    angleChanger,
-    setAngleChanger,
     rotateAngle,
     setRotateAngle,
     gameOver,
     setGameOver,
     setWin,
-    setRouletteStyle
+    segmentAngle,
+    numberOfSegments,
   } = rouletteStorage;
+
+  const handleSpinResult = (sector) => {
+    // console.log('sector', sector);
+    if (oddBid) {
+      if (sector > 0 && sector % 2 === 0) {
+        setResult(-oddBid);
+        setWin(false);
+      } else if (sector > 0 && sector % 2 !== 0) {
+        setResult(oddBid);
+        setWin(true);
+      } else {
+        setResult(-oddBid);
+        setWin(false);
+      }
+    } else if (evenBid) {
+      if (sector > 0 && sector % 2 === 0) {
+        setResult(evenBid);
+        setWin(true);
+      } else if (sector > 0 && sector % 2 !== 0) {
+        setResult(-evenBid);
+        setWin(false);
+      } else {
+        setResult(-evenBid);
+        setWin(false);
+      }
+    }
+  };
+
   const spinHandle = (e) => {
     if (result && (oddBid || evenBid) && e.pressed) {
-      setAngleChanger(Math.floor(Math.random() * 14400) - 7199);
-      setRotateAngle(angleChanger);
-      console.log("angleChanger", angleChanger);
+      let angleC = Math.floor(Math.random() * 14400) - 7199;
+      let rotateAnglePrev = rotateAngle + angleC;
       setSpinStarted(true);
-      console.log("spinStarted", spinStarted);
-      console.log("angleChanger", angleChanger, "spinStarted", spinStarted);
-      setRouletteStyle(
-        "transition",
-        `transform ${Math.abs(angleChanger)}ms ease-in-out`
-      );
-      setRouletteStyle("transform", `rotate(${rotateAngle}deg) scale(2)`);
+      // console.log('spinStarted', spinStarted);
+      // console.log('angleC', angleC, 'spinStarted', spinStarted, 'rotateAngle', rotateAngle, rotateAnglePrev);
+      wheelRef.current.style.transition = `transform ${Math.abs(angleC)}ms ease-in-out`;
+      wheelRef.current.style.transform = `rotate(${rotateAnglePrev}deg) scale(2)`;
+      setWin(null);
+      setRotateAngle(angleC);
+      const tyty = setTimeout(() => {
+        let pureAngle = (rotateAnglePrev % 360) - segmentAngle / 2;
+        if (pureAngle < 0) {
+          pureAngle = 360 + pureAngle;
+        }
+        let pureAnglePerSegmentAngle = Math.floor(pureAngle / segmentAngle);
+        if (pureAnglePerSegmentAngle === numberOfSegments - 1) {
+          handleSpinResult(0);
+        } else {
+          handleSpinResult(pureAnglePerSegmentAngle + 1);
+        }
+      }, Math.abs(angleC));
+      setSpinStarted(false);
+      return () => {
+        clearTimeout(tyty);
+      };
     }
   };
   return (
@@ -106,14 +140,14 @@ export const Tableau = observer(() => {
       <div
         onClick={() => {
           if (double && evenBid > 0) {
-            setBids("evenBid", "double");
+            setBids('evenBid', 'double');
           } else {
-            setBids("evenBid", "plus");
+            setBids('evenBid', 'plus');
           }
           if (gameOver) setGameOver(false);
           setWin(null);
           if (oddBid) {
-            setBids("oddBid", "zero");
+            setBids('oddBid', 'zero');
           }
         }}
         className="spin-button"
@@ -123,14 +157,14 @@ export const Tableau = observer(() => {
       <div
         onClick={() => {
           if (double && oddBid > 0) {
-            setBids("oddBid", "double");
+            setBids('oddBid', 'double');
           } else {
-            setBids("oddBid", "plus");
+            setBids('oddBid', 'plus');
           }
           if (gameOver) setGameOver(false);
           setWin(null);
           if (evenBid) {
-            setBids("evenBid", "zero");
+            setBids('evenBid', 'zero');
           }
         }}
         className="spin-button"
@@ -162,7 +196,7 @@ export const Tableau = observer(() => {
         onClick={() => {
           setDouble();
         }}
-        className={`spin-button ${double ? "spin-button-active" : ""}`}
+        className={`spin-button ${double ? 'spin-button-active' : ''}`}
       >
         x2
       </div>
@@ -170,81 +204,15 @@ export const Tableau = observer(() => {
   );
 });
 
-export const Wheel = observer(() => {
-  const {
-    oddBid,
-    evenBid,
-    setResult,
-    setWin,
-    numberOfSegments,
-    setNumberOfSegments,
-    segmentAngle,
-    setSegmentAngle,
-    rotateAngle,
-    angleChanger,
-    spinStarted,
-    setSpinStarted,
-    wheelRef,
-    setwheelref
-  } = rouletteStorage;
-
-  const wheelRef1 = React.useRef();
-  if (wheelRef1.current) {
-    setwheelref(wheelRef1.current);
-    // console.log(wheelRef);
-  }
-  const handleSpinResult = (sector) => {
-    console.log("sector", sector);
-    if (oddBid) {
-      if (sector > 0 && sector % 2 === 0) {
-        setResult(-oddBid);
-        setWin(false);
-      } else if (sector > 0 && sector % 2 !== 0) {
-        setResult(oddBid);
-        setWin(true);
-      } else {
-        setResult(-oddBid);
-        setWin(false);
-      }
-    } else if (evenBid) {
-      if (sector > 0 && sector % 2 === 0) {
-        setResult(evenBid);
-        setWin(true);
-      } else if (sector > 0 && sector % 2 !== 0) {
-        setResult(-evenBid);
-        setWin(false);
-      } else {
-        setResult(-evenBid);
-        setWin(false);
-      }
-    }
-  };
-
-  React.useEffect(() => {
-    console.log("wheel effect", rotateAngle);
-    setWin(null);
-    const tyty = setTimeout(() => {
-      let pureAngle = (rotateAngle % 360) - segmentAngle / 2;
-      if (pureAngle < 0) {
-        pureAngle = 360 + pureAngle;
-      }
-      let pureAnglePerSegmentAngle = Math.floor(pureAngle / segmentAngle);
-      if (pureAnglePerSegmentAngle === numberOfSegments - 1) {
-        handleSpinResult(0);
-      } else {
-        handleSpinResult(pureAnglePerSegmentAngle + 1);
-      }
-    }, Math.abs(angleChanger));
-    setSpinStarted(false);
-    return () => {
-      clearTimeout(tyty);
-    };
-  }, [spinStarted, rotateAngle]);
-
+export const Wheel = observer(({ wheelRef }) => {
   return (
     <div>
       <span className="arrow"></span>
-      <div ref={wheelRef1} className="wheel">
+      <div
+        ref={wheelRef}
+        className="wheel"
+        style={{ transition: 'transform 1000ms ease-in-out 0s', transform: 'rotate(0deg) scale(2)' }}
+      >
         <div className="segment one"> </div>
         <div className="segment two"> </div>
         <div className="segment three"> </div>
@@ -259,43 +227,28 @@ export const Wheel = observer(() => {
   );
 });
 
-const Results = observer(() => {
+const Results = observer(({ wheelRef }) => {
   // console.log("rouletteStorage", rouletteStorage);
-  let {
-    gameOver,
-    result,
-    oddBid,
-    evenBid,
-    win,
-    wheelRef,
-    setNumberOfSegments,
-    numberOfSegments,
-    setSegmentAngle,
-    segmentAngle
-  } = rouletteStorage;
+  let { gameOver, result, oddBid, evenBid, win, setNumberOfSegments, numberOfSegments, setSegmentAngle, segmentAngle } =
+    rouletteStorage;
 
   React.useEffect(() => {
-    console.log("wheelRef", wheelRef);
+    // console.log('wheelRef', wheelRef.current);
 
     if (wheelRef) {
-      const uuuu = wheelRef.querySelectorAll(".segment");
-      console.log("wheelRef", wheelRef, "uuuu", uuuu.length);
+      const uuuu = wheelRef.current.querySelectorAll('.segment').length;
+      // console.log('wheelRef', wheelRef.current, 'uuuu', uuuu.length);
 
-      setNumberOfSegments(9);
-      setSegmentAngle(360 / 9);
-      console.log(
-        "numberOfSegments",
-        numberOfSegments,
-        "segmentAngle",
-        segmentAngle
-      );
+      setNumberOfSegments(uuuu);
+      setSegmentAngle(360 / uuuu);
+      // console.log('numberOfSegments', numberOfSegments, 'segmentAngle', segmentAngle);
     }
   }, [numberOfSegments]);
   React.useEffect(() => {
     // console.log("result effect");
     if (!gameOver) {
       // console.log("stored");
-      localStorage.setItem("myrouletteresult", JSON.stringify({ result }));
+      localStorage.setItem('myrouletteresult', JSON.stringify({ result }));
     }
   }, [result, gameOver]);
 
@@ -305,22 +258,20 @@ const Results = observer(() => {
       {result > 0 && <span>Счет: {result}</span>}
       {oddBid > 0 && <span>, ставка на нечет: {oddBid}</span>}
       {evenBid > 0 && <span>, ставка на чет: {evenBid}</span>}
-      {result > 0 && evenBid === 0 && oddBid === 0 && (
-        <span>, ваша ставка? </span>
-      )}
-      <span>
-        {win === true ? ` - выигрыш 😆` : win === false ? ` - проигрыш 😟` : ``}
-      </span>
+      {result > 0 && evenBid === 0 && oddBid === 0 && <span>, ваша ставка? </span>}
+      <span>{win === true ? ` - выигрыш 😆` : win === false ? ` - проигрыш 😟` : ``}</span>
     </div>
   );
 });
 
 export const Container = observer(() => {
+  const wheelRef = React.useRef();
+
   let { result, setResult } = rouletteStorage;
 
   React.useEffect(() => {
     if (result === 0) {
-      const resFromStorage = localStorage.getItem("myrouletteresult");
+      const resFromStorage = localStorage.getItem('myrouletteresult');
       if (resFromStorage) {
         setResult(JSON.parse(resFromStorage).result);
       }
@@ -329,9 +280,9 @@ export const Container = observer(() => {
 
   return (
     <>
-      <Wheel />
-      <Results />
-      <Tableau />
+      <Wheel wheelRef={wheelRef} />
+      <Results wheelRef={wheelRef} />
+      <Tableau wheelRef={wheelRef} />
     </>
   );
 });
